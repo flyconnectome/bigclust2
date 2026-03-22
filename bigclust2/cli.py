35d0723083bb1149ef90e69d09333c5d0bcd2a32
Philipp Schlegel <observing@web.de>
2026-03-22 11:38:48 +0000

    CLI: add argparsing

--- bigclust2/cli.py
+++ bigclust2/cli.py
@@ -1,6 +1,16 @@
-import sys
+import argparse
 
-from .main import main
+from .gui import main
+from .__version__ import __version__
+
 
 def app():
+    parser = argparse.ArgumentParser(
+        prog="bigclust2", description="A Python package for inspecing and visualising large clusterings"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+
+    parser.parse_args()
     main()

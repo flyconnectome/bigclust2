ac4a5d79dbf08d526f54c6482dcaa042bf75f5fe
Philipp Schlegel <observing@web.de>
2026-03-24 09:21:16 +0000

    suppress octarine warning

--- bigclust2/__init__.py
+++ bigclust2/__init__.py
@@ -1,1 +1,15 @@
+import os
+import sys
+
+# Suppress octarine's warning about event loop potentially not running
+os.environ['OCTARINE_CHECK_LOOP'] = '0'
+
+# Some feedback for the user that stuff is happening, especially when running from the command line
+__context__ = "cli" if 'bigclust2' in sys.argv[0] else "gui"
+if __context__ == "cli":
+    print("Loading bigclust2... ", end="", flush=True)
+
 from .gui import main
+
+if __context__ == "cli":
+    print("done.", flush=True)
